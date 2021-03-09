c4707a7ff4bc1cd34236ce935208219279339a40
TanKARASU <karasutan@gmail.com>
2021-03-09 20:44:36 +0100

    début mise en place header

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import HeaderMenu from "./container/HeaderMenu";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>test up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <HeaderMenu  />
     </View>
   );
 }
@@ -14,8 +16,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  header: {
+    backgroundColor: "green",
+    color: "red",
   },
 });
