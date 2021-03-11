2991ef27a3b4a2f9187680e942bd6cf07cf90294
yegabebar <m.barbegayet@gmail.com>
2021-03-11 13:15:16 +0100

    about page ok

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import HomeMenu from "./container/HomeMenu";
-//import AddressForm from "./AddressForm";
+
+
 
 export default function App() {
   return <HomeMenu />;
-  //return <AddressForm/>
 }
 
 const styles = StyleSheet.create({
