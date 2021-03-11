5febfa07f5268fcaf2272854f3bc5166252bcfec
TanKARASU <karasutan@gmail.com>
2021-03-12 00:01:28 +0100

    bugtix sur chemin relatif

--- restaurant.js
+++ restaurant.js
@@ -1,213 +1,241 @@
-const db = [ 
-   {
-   restaurantName : "Wow Galette",
-   description: "la meilleur galette de toute la terre et bien plus encore!",
-   promo: "Spend €35, get 15% off",
-   distance: 2.8,
-   fee: "2.99 delivery",
-   note: 5.0 ,
-   category: "Sandwich",
-   listsDish : [ {
-         name : "Galette viande boeuf",
-         price : 10.50,
-         description : "Viande bovine hachée origine france, tabouleh,crème d’ail. 1 Beignet au choix",
-         url: require("./assets/images/resto/galetteViande.png"),
-         popular : true
+const db = [
+  {
+    restaurantName: "Wow Galette",
+    description:
+      "la meilleur galette de toute la terre et bien plus encore!",
+    promo: "Spend €35, get 15% off",
+    distance: 2.8,
+    fee: "2.99 delivery",
+    note: 5.0,
+    category: "Sandwich",
+    listsDish: [
+      {
+        name: "Galette viande boeuf",
+        price: 10.5,
+        description:
+          "Viande bovine hachée origine france, tabouleh,crème d’ail. 1 Beignet au choix",
+        url: require("./assets/images/restos/galetteViande.png"),
+        popular: true,
       },
       {
-         name: "Assiette variétés",
-         price: 9.00,
-         description: "1 brochette + 1 pain libanais.",
-         url: require("./assets/images/resto/galetteViande.png"),
-         popular : false
+        name: "Assiette variétés",
+        price: 9.0,
+        description: "1 brochette + 1 pain libanais.",
+        url: require("./assets/images/restos/galetteViande.png"),
+        popular: false,
       },
       {
-         name: "Assiette 5 variétés",
-         price: 13.00,
-         description: "2 brochettes  + 4 accompagnements",
-         url: require("./assets/images/resto/ass5var.png"),
-         popular : true
-      }],
-   },
-   {
-      restaurantName : "La Florentine",
-      description: "Pizzeria traditionnelle italienne, toutes nos Pizza sont faites maison préparées à base de produits frais et cuite au feux de bois",
-      promo: "Spend €35, get 15% off",
-      distance: 0.5,
-      fee: "2.99 delivery",
-      note: 3.0,
-      category: "Pizza",
-      listsDish : [ {
-         name: "Vegetarian",
-         price: 12,
-         description: "Tomate, mozzarella, champignons, aubergines, poivrons, courgettes et artichaut",
-         url: require("./assets/images/restos/laVeg.png"),
-         popular : true
+        name: "Assiette 5 variétés",
+        price: 13.0,
+        description: "2 brochettes  + 4 accompagnements",
+        url: require("./assets/images/restos/ass5var.png"),
+        popular: true,
+      },
+    ],
+  },
+  {
+    restaurantName: "La Florentine",
+    description:
+      "Pizzeria traditionnelle italienne, toutes nos Pizza sont faites maison préparées à base de produits frais et cuite au feux de bois",
+    promo: "Spend €35, get 15% off",
+    distance: 0.5,
+    fee: "2.99 delivery",
+    note: 3.0,
+    category: "Pizza",
+    listsDish: [
+      {
+        name: "Vegetarian",
+        price: 12,
+        description:
+          "Tomate, mozzarella, champignons, aubergines, poivrons, courgettes et artichaut",
+        url: require("./assets/images/restos/laVeg.png"),
+        popular: true,
       },
       {
-         name: "Chef",
-         price: 12,
-         description: "Tomate, mozzarella, merguez, oignons, œuf",
-         url: require("./assets/images/restos/chef.png"),
-         popular : false
+        name: "Chef",
+        price: 12,
+        description: "Tomate, mozzarella, merguez, oignons, œuf",
+        url: require("./assets/images/restos/chef.png"),
+        popular: false,
       },
       {
-         name: "Florine",
-         price: 12,
-         description: "Tomate, mozzarella, merguez, oignons, œuf",
-         url: require("./assets/images/restos/florentine.png"),
-         popular : false
-      }],
-   },
-   {
-      restaurantName : "California sushi",
-      description: "Venez découvrir les meilleurs sushis de Paris chez California Sushi ! 🍣 ",
-      promo: "Spend €50 - Free delivery",
-      distance: 3.2,
-      fee: "1.50 delivery",
-      note: 4.9 ,
-      category: "Sushi",
-      listsDish : [ {
-         name: "Choux crevettes",
-         price: 9,
-         description: "Choux crevettes sucrée",
-         url: require("./assets/images/restos/chuCrev.png"),
-         popular : false
+        name: "Florine",
+        price: 12,
+        description: "Tomate, mozzarella, merguez, oignons, œuf",
+        url: require("./assets/images/restos/florentine.png"),
+        popular: false,
+      },
+    ],
+  },
+  {
+    restaurantName: "California sushi",
+    description:
+      "Venez découvrir les meilleurs sushis de Paris chez California Sushi ! 🍣 ",
+    promo: "Spend €50 - Free delivery",
+    distance: 3.2,
+    fee: "1.50 delivery",
+    note: 4.9,
+    category: "Sushi",
+    listsDish: [
+      {
+        name: "Choux crevettes",
+        price: 9,
+        description: "Choux crevettes sucrée",
+        url: require("./assets/images/restos/chuCrev.png"),
+        popular: false,
       },
       {
-         name: "Crevette avocat sushi",
-         price: 7,
-         description: "Tomate, mozzarella, merguez, oignons, œuf",
-         url: require("./assets/images/restos/crev.png"),
-         popular : true
-      }],
-   },
-   {
-      restaurantName : "Wadam sushi",
-      description: "Sushi pour toute la famille",
-      promo: "Spend €50 get 2€ off",
-      distance: 0.7,
-      fee: "1.99 delivery",
-      note: 4.7 ,
-      category: "Sushi",
-      listsDish : [ {
-         name: "Le tigre qui pleur",
-         price: 8.90,
-         description: "Sushi roll avec gingembre",
-         url: require("./assets/images/restos/loc.png"),
-         popular : false
+        name: "Crevette avocat sushi",
+        price: 7,
+        description: "Tomate, mozzarella, merguez, oignons, œuf",
+        url: require("./assets/images/restos/crev.png"),
+        popular: true,
+      },
+    ],
+  },
+  {
+    restaurantName: "Wadam sushi",
+    description: "Sushi pour toute la famille",
+    promo: "Spend €50 get 2€ off",
+    distance: 0.7,
+    fee: "1.99 delivery",
+    note: 4.7,
+    category: "Sushi",
+    listsDish: [
+      {
+        name: "Le tigre qui pleur",
+        price: 8.9,
+        description: "Sushi roll avec gingembre",
+        url: require("./assets/images/restos/loc.png"),
+        popular: false,
       },
       {
-         name: "Boeuf loc loc",
-         price: 11.50,
-         description: "riz avec morceaux de boeuf ",
-         url: require("./assets/images/restos/tigre.png"),
-         popular : true
-      }],
-   },
-   {
-      restaurantName : "Chicken Spot",
-      description:'les meilleurs burgers du meilleur restaurant américain',
-      promo: "Spend €35, get 15% off",
-      distance: 4.08,
-      fee: 0 ,
-      note: 4.4 ,
-      category: "Sandwich",
-   
-      listsDish : [ {
-         name : "Chicken Mixte",
-         price : 9.80,
-         description : "2 pièces de poulet + 3 wings (ailes et pilons) marinés et cuisinés d'après la recette originale CS. Avec : - 2 sauces au choix, - 1 portion de frite, - 1 boisson 33 cl.",
-         url: require("./assets/images/resto/ChickenMixte.png"),
-         popular : false,
+        name: "Boeuf loc loc",
+        price: 11.5,
+        description: "riz avec morceaux de boeuf ",
+        url: require("./assets/images/restos/tigre.png"),
+        popular: true,
       },
-      {
-         name : "Hot'n'Spicy Wings",
-         price : 11.20,
-         description : "10 wings (ailes et pilons) marinés et cuisinés d'après la recette originale Chicken Spot. Avec : - 2 sauces au choix, - 1 portion de frite, - 1 boisson 33 cl.",
-         url: require("./assets/images/resto/HotSpicy.png"),
-         popular : true,
-      },
-      {
-         name : "Spicy Strips",
-         price : 11.20,
-         description : "15 morceaux de poulet tendre marinés et cuisinés à la sauce Tikka spéciale CS (non épicé). Avec : - 2 sauces au choix, - 1 portion de frite, - 1 boisson 33 cl.",
-         url: require("./assets/images/resto/SpicyStrips.png"),
-         popular : false,
-      }]
-   },
-      
-   {
-      restaurantName : "Fête à Crêpe",
-      description:'Le menu Fête à Crêpe vous propose des crêpes décalées, bien loin de la sempiternelle galette bretonne.',
-      promo: "Spend €15, get 10% off",
-      distance: 0.45,
-      fee: 0 ,
-      note: 4.4 ,
-      category: "Dessert",
-   
-      listsDish : [ {
-         name : "Chiken twin crêpe",
-         price : 11.40,
-         description : "Taille au choix. Algérienne, boursin, oignons frits, chorizo, mozzarella, tenders",
-         url: require("./assets/images/resto/ChickenMixte.png"),
-         popular : false,
-      },
-      {
-         name : "Frenchy crêpe",
-         price : 9.90,
-         description : "T-Taille au choix. Crème fraîche, pommes de terre, champignons, salade, raclette, poulet fumé et lardons de volaille",
-         url: require("./assets/images/resto/FranchyCrepe.png"),
-         popular : false,
-      }],
-   },          
-   {
-      restaurantName : "CHIC'N TASTY",
-      description:"Venez découvrir nos poulets rôtis, notre riz thaï, nos frites cuites façons belges ainsi que nos délicieuses bananes plantins.",
-      distance: 1.99,
-      promo: "",
-      fee: 0.45 ,
-      note: 0,
-      category: "Dessert",
-   
-      listsDish : [{
-         name : "Tiramisus nutella oreo",
-         price : 3.50,
-         description : "Tiramisus nutella oreo",
-         url: require("./assets/images/resto/TiramissusNutellaOreo.png"),
-         popular : false,
-      },
-      {
-         name : "Tiramisus nutella cookies",
-         price : 3.50,
-         description : "Tiramisus nutella coockies",
-         url: require("./assets/images/resto/Tirnuaco.png"),
-         popular : false,
-      }],
-   },  
-   {
-      restaurantName : "Tipsy's",
-      description: "L'alcool est dangereux pour la santé, à consommer avec modération.",
-      promo: "Spend €20 get 1.5€ off",
-      distance: 1.2,
-      fee: "2.99 delivery",
-      note: 4.4 ,
-      category: "Epicerie",
-      listsDish : [ {
-         name: "Vodka",
-         price: 30.00,
-         description: "Smirnoff 70 CL",
-         url: require("./assets/images/restos/loc.png"),
-         popular : false
-      },
-      {
-         name: "Olives",
-         price: 5.00,
-         description: "Olives farcies",
-         url: require("./assets/images/restos/tigre.png"),
-         popular : false
-      }],
-   },
+    ],
+  },
+  {
+    restaurantName: "Chicken Spot",
+    description:
+      "les meilleurs burgers du meilleur restaurant américain",
+    promo: "Spend €35, get 15% off",
+    distance: 4.08,
+    fee: 0,
+    note: 4.4,
+    category: "Sandwich",
 
-]
+    listsDish: [
+      {
+        name: "Chicken Mixte",
+        price: 9.8,
+        description:
+          "2 pièces de poulet + 3 wings (ailes et pilons) marinés et cuisinés d'après la recette originale CS. Avec : - 2 sauces au choix, - 1 portion de frite, - 1 boisson 33 cl.",
+        url: require("./assets/images/restos/ChickenMixte.png"),
+        popular: false,
+      },
+      {
+        name: "Hot'n'Spicy Wings",
+        price: 11.2,
+        description:
+          "10 wings (ailes et pilons) marinés et cuisinés d'après la recette originale Chicken Spot. Avec : - 2 sauces au choix, - 1 portion de frite, - 1 boisson 33 cl.",
+        url: require("./assets/images/restos/HotSpicy.png"),
+        popular: true,
+      },
+      {
+        name: "Spicy Strips",
+        price: 11.2,
+        description:
+          "15 morceaux de poulet tendre marinés et cuisinés à la sauce Tikka spéciale CS (non épicé). Avec : - 2 sauces au choix, - 1 portion de frite, - 1 boisson 33 cl.",
+        url: require("./assets/images/restos/SpicyStrips.png"),
+        popular: false,
+      },
+    ],
+  },
 
+  {
+    restaurantName: "Fête à Crêpe",
+    description:
+      "Le menu Fête à Crêpe vous propose des crêpes décalées, bien loin de la sempiternelle galette bretonne.",
+    promo: "Spend €15, get 10% off",
+    distance: 0.45,
+    fee: 0,
+    note: 4.4,
+    category: "Dessert",
+
+    listsDish: [
+      {
+        name: "Chiken twin crêpe",
+        price: 11.4,
+        description:
+          "Taille au choix. Algérienne, boursin, oignons frits, chorizo, mozzarella, tenders",
+        url: require("./assets/images/restos/ChickenMixte.png"),
+        popular: false,
+      },
+      {
+        name: "Frenchy crêpe",
+        price: 9.9,
+        description:
+          "T-Taille au choix. Crème fraîche, pommes de terre, champignons, salade, raclette, poulet fumé et lardons de volaille",
+        url: require("./assets/images/restos/FranchyCrepe.png"),
+        popular: false,
+      },
+    ],
+  },
+  {
+    restaurantName: "CHIC'N TASTY",
+    description:
+      "Venez découvrir nos poulets rôtis, notre riz thaï, nos frites cuites façons belges ainsi que nos délicieuses bananes plantins.",
+    distance: 1.99,
+    promo: "",
+    fee: 0.45,
+    note: 0,
+    category: "Dessert",
+
+    listsDish: [
+      {
+        name: "Tiramisus nutella oreo",
+        price: 3.5,
+        description: "Tiramisus nutella oreo",
+        url: require("./assets/images/restos/TiramissusNutellaOreo.png"),
+        popular: false,
+      },
+      {
+        name: "Tiramisus nutella cookies",
+        price: 3.5,
+        description: "Tiramisus nutella coockies",
+        url: require("./assets/images/restos/Tirnuaco.png"),
+        popular: false,
+      },
+    ],
+  },
+  {
+    restaurantName: "Tipsy's",
+    description:
+      "L'alcool est dangereux pour la santé, à consommer avec modération.",
+    promo: "Spend €20 get 1.5€ off",
+    distance: 1.2,
+    fee: "2.99 delivery",
+    note: 4.4,
+    category: "Epicerie",
+    listsDish: [
+      {
+        name: "Vodka",
+        price: 30.0,
+        description: "Smirnoff 70 CL",
+        url: require("./assets/images/restos/loc.png"),
+        popular: false,
+      },
+      {
+        name: "Olives",
+        price: 5.0,
+        description: "Olives farcies",
+        url: require("./assets/images/restos/tigre.png"),
+        popular: false,
+      },
+    ],
+  },
+];
